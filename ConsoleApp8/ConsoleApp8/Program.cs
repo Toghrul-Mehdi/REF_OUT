da8070c05e54dd27431b216da91ad7c2c564b7fa
Toghrul-Mehdi <togrulcm-bp215@code.edu.az>
2024-10-11 11:20:45 +0400

    REF_OUT

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine("Indiki array:");
+            foreach (int i in array) 
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine("");
+            InsertArray(ref array,7,8,9);
+        }
+        static void InsertArray(ref int[] array, params int[] deyer) 
+        {
+            int[] newArray = new int[array.Length+deyer.Length];
+            array.CopyTo( newArray, 0 );
+
+            for (int i = 0; i < deyer.Length; i++)
+            {
+                newArray[array.Length + i] = deyer[i]; 
+            }
+            Console.WriteLine("Yeni array:");
+            foreach (int i in newArray)
+            {
+                Console.Write(i+" ");
+            } 
+            newArray=array;
+            Console.WriteLine("");
+            Console.WriteLine("Arrayi kohne qiymetine qaytaraq:");
+            foreach (int i in newArray)
+            {
+                Console.Write(i+" ");
+            }
+        }
+    }
+
+
+}
